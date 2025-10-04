b55fb63ef56c55388802b6532acd34d148134b03
Thomas-Shelby-1 <ameysawant986@outlook.com>
2025-10-05 01:09:27 +0530

    refactor: remove old scaffold and add new Hackon codebase

--- hackon/scripts/ingest_youtube.py
+++
@@ -1,5 +0,0 @@
-from youtube_transcript_api import YouTubeTranscriptApi
-tid = "VIDEO_ID_HERE"
-tx = YouTubeTranscriptApi.get_transcript(tid, languages=["en"])
-full = " ".join([t["text"] for t in tx])
-print(full[:400], "...")
